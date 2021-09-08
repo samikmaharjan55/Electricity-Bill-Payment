4b416c4afa830e53b97ee0e1691b22109b0f986a
Samik Maharjan <samikmaharjan55@gmail.com>
2021-09-08 20:41:13 +0545

    Updated

--- component/footer.php
+++ component/footer.php
@@ -1,14 +1,13 @@
 <hr/>
+<div class="container">
 <div class="row">
-    <div class="container">
-<div class="footer">
-<div class="col-md-10 col">
-    About Us</div>
-    <div class="col-md-2 col">
-    Contact Us</div>
+    
+
+<div class="col-md-9 col">
+<div class="footer">About Us</div></div>
+    <div class="col-md-3 col">
+    <div class="footer">    Contact Us</div></div>
 </div>
 </div>
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </html>
--- component/header.php
+++ component/header.php
@@ -5,8 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Electricity Bill Payment</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href = "css/bootstrap.min.css" rel="stylesheet">
+    <script src = "js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="CSS/style.css">
-    <div id='title'>Electricity Bill Payment</div>
+
+    
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="bill.php">
+    <img src="img/logo.png" width="50" height="50" alt="">
+  </a>
+</nav>
+  <a class="navbar-brand" href="bill.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      
+      <li class="nav-item">
+        <a class="nav-link" href="billQuery.php">BillQuery</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
     <hr/>
 </head>
